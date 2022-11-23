2eaed150dfde2b3a84130a35d9073600f578c9d1
Julio Cesar Pereira <julliopereira@gmail.com>
2022-11-23 12:55:11 -0300

    start

---
+++ ping_show_config.py
@@ -0,0 +1,30 @@
+#from netmiko import ConnectHandler
+import os
+
+def ping_dev(ip):
+    # Pinga dispositivo e grava status na variavel res
+    res = os.system(f"ping -c 1 {ip} -i 0.2 -W 0.3 > /dev/null ; echo $?")
+
+def cisco_config(ip):
+    # Configuração de hostname
+    cisco = { 
+        'device_type': 'cisco_ios',
+        'host': "ip",
+        'username': 'cisco',
+        'password': 'cisco',
+        'port': '22',
+        'secret': 'cisco'
+    }
+    print(cisco)
+
+# Ler arquivo e gravar em lista
+with open("devices.txt", "r") as arquivo:
+    ip = arquivo.readlines()
+
+# Para cada valor da lista 
+for ip in ip:
+    ip = ip.rstrip('\n')
+    val = ping_dev(ip)
+    # Se pingar então configura hostname
+    if val == '0':
+        cisco_config(ip)
